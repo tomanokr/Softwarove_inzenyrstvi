3d78dd6627b2e668d45f0718cf5bf0a7f05645ec
Kristýna <37155274+tomanokr@users.noreply.github.com>
2019-10-01 16:58:47 +0200

    Add files via upload

---
+++ 2019_ZS/A3AFW/cviceni_3/Carousel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Cviceni_treti
+{
+    public class Carousel : ContentView
+    {
+        public Carousel()
+        {
+            Content = new Label { Text = "Hello ContentView" };
+        }
+    }
+}
+
---
+++ 2019_ZS/A3AFW/cviceni_3/Carousel.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Cviceni_treti
+{
+    public partial class Carousel : CarouselPage
+    {
+        public Carousel()
+        {
+            InitializeComponent();
+            
+    }
+}
